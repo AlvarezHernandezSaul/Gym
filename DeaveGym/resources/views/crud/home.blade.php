435d77bc8a4570ac7405dbebb486e1b4889a769b
AlvarezHernandezSaul <al222110813@gmail.com>
2023-03-24 11:38:53 -0600

    pruebaREP

---
+++ DeaveGym/resources/views/crud/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'view1')
+
+@section('content')
+
---
+++ DeaveGym/resources/views/view1.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'view1')
+
+@section('content')
+
+
+<div class="container position-absolute top-50 start-25 translate-middle-y" >
+    <div>
+        
+<img src="{{ asset('img/brand/portada.jpg') }}" width="1300" height="600" class="rounded float-start" alt="..." style="margin-top: 5cm;">
+<div>
+<video width="1300" height="600" controls autoplay>
+  <source src="{{ asset('img/brand/Bienvenida1.mp4') }}" type="video/mp4">
+</video>
+</div>
+
+</div>
+
+
+</div>
+    
