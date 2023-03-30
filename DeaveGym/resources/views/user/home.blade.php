dc9f46ddfc44d839dc465952747a937fe00ccdde
AlvarezHernandezSaul <al222110813@gmail.com>
2023-03-29 21:48:31 -0600

    acept

--- DeaveGym/resources/views/user/home.blade.php
+++ DeaveGym/resources/views/user/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.user')
 @extends('layouts.app')
 
-<<<<<<< HEAD
 @section('title', 'Home')
-=======
 <link rel="icon" type="image/x-icon" href="{{ asset('img/brand/Dv.png') }}">
 @section('log')
->>>>>>> 90d5389133811ceda9a05b90dbeac9e529bf2e99
 
 @section('title', 'view1')
 @section('content')
 
-<<<<<<< HEAD
 <div class="container position-absolute" >
 <div class="container" style="margin-top: 3cm;">
 <h1 >Home Users</h1>
 </div>
 </div>
-=======
 
 @section('tabla')
 @endsection
@@ -26,4 +21,3 @@
 
 
 
->>>>>>> 90d5389133811ceda9a05b90dbeac9e529bf2e99
